732bae631af34985457eef83c11b530fd78c0b0a
Yining2003 <ysun0531@uni.sydney.edu.au>
2025-11-11 14:33:52 +1100

    change thickness level & fit with the widow size

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ let gravityDirection = 1;
 let ground = 750;
 let topY = 20;
 let noisePoints = [];
+let scaleFactor;
 
 class Segment{
   constructor(x,y,length,angle,level){
@@ -20,13 +21,15 @@ class Segment{
     if(this.level === 1){
       this.thickness = 15;
     } else if(this.level === 2){
-      this.thickness = 8;
-    } else {
-      this.thickness = 4;
+      this.thickness = 10;
+    } else if(this.level === 3){
+      this.thickness = 7;
+    } else{
+      this.thickness = 4
     }
 
     this.swayAmp = random(1,3);
-    this.swaySpeed = random(0.05,0.05);
+    this.swaySpeed = random(0.2, 0.2);
 
     this.x2 = this.x + cos(this.angle) * this.length;
     this.y2 = this.y - sin(this.angle) * this.length;
@@ -136,17 +139,18 @@ function generateTree(x, y, length, angle, level){
 }
 
 function setup() {
-  createCanvas(DESIGN_W, DESIGN_H); 
+  createCanvas(windowWidth, windowHeight); 
   frameRate(60);  
+  scaleFactor = min(windowWidth/ DESIGN_W, windowHeight/ DESIGN_H);
 
   for (let i = 0; i < 1000; i++){
     noisePoints.push({
-      x: random(-100,width),
+      x: random(-1000,width),
       y: random(0, 650),
       c:[random(100,180), random(150,200), random(200,255), random(80,150)]
     });
   }
-  generateTree(300, 650, 120, -PI / 2, 1);
+  generateTree(300, 650, 200, PI / 2, 1);
 
   const appleSeeds = [
     { x:125, y:200, color:[220, 80, 80] },
@@ -167,7 +171,11 @@ function setup() {
 function draw(){
   //base background
   background(60,80,120);
- 
+  
+  push();
+  scale(scaleFactor);
+  translate((width / scaleFactor - DESIGN_W)/ 2, (height/ scaleFactor - DESIGN_H)/2);
+
   noStroke();
   for (let p of noisePoints){
     fill(p.c[0],p.c[1],p.c[2],p.c[3]);
@@ -177,7 +185,7 @@ function draw(){
   fill(40,140,90);
   rect(0,650,600,100);
   stroke(0);
-  strokeWeight(3);
+  strokeWeight(5);
   noFill();
   rect(0,650,600,100);
   noStroke();
@@ -185,7 +193,7 @@ function draw(){
   //yellow base
   fill(240,210,60);
   stroke(0);
-  strokeWeight(2.5);
+  strokeWeight(10);
   rect(125,625,350,75);
   noStroke();
 
@@ -269,6 +277,12 @@ function draw(){
     text("Press SPACE to change gravity (now ↑ ↑ ↑)",20,785);
   }
     text("- Let Newton be confused ! ! ! -",240,30);
+    pop();
+}
+
+function fitWidow(){
+  resizeCanvas(windowWidth,windowHeight);
+  scaleFactor = min(windowWidth / DESIGN_W, windowHeight/ DESIGN_H);
 }
 
 function keyPressed(){

930285e0b1815ffb8f1b0e9d32a8bd8c02ed6642
Yining2003 <ysun0531@uni.sydney.edu.au>
2025-11-15 20:39:43 +1100

    Add comments in the code.

--- sketch.js
+++ sketch.js
@@ -3,13 +3,14 @@ const DESIGN_W = 600;
 
 let branches = [];
 let apples = [];
-let gravity = 0.2;
+let gravity = 0.2;  
 let gravityDirection = 1;
 let ground = 750;
 let topY = 20;
 let noisePoints = [];
 let scaleFactor;
 
+//Segment Class (Tree Branch).
 class Segment{
   constructor(x,y,length,angle,level){
     this.x = x;
@@ -18,6 +19,7 @@ class Segment{
     this.angle = angle;
     this.level = level;
 
+  //Different thickness for different branch level.
     if(this.level === 1){
       this.thickness = 15;
     } else if(this.level === 2){
@@ -27,20 +29,20 @@ class Segment{
     } else{
       this.thickness = 4
     }
-
+  // Small waving animation values.
     this.swayAmp = random(1,3);
     this.swaySpeed = random(0.2, 0.2);
-
+  // Calculate the end point of the branch based on angle.
     this.x2 = this.x + cos(this.angle) * this.length;
     this.y2 = this.y - sin(this.angle) * this.length;
   }
 
   
   draw(){
+    // Set branches' color
     stroke(0);
-    // ↑ branches' color
     strokeWeight(this.thickness);
-    
+    // Small animation using sin() to simulate "wind".
     let sway = sin(frameCount* this.swaySpeed + this.y * 0.05)* this.swayAmp;
 
     let newX = this.x + cos(this.angle + radians(sway * 0.5)) * this.length;
@@ -50,7 +52,7 @@ class Segment{
     //let branches silghtly wave.
   }
 }
-
+// Apple class(generate, sway, drop)
 class Apple {
   constructor(x,y,color){
     this.stratX = x;
@@ -61,13 +63,13 @@ class Apple {
     this.color = color;
     this.state = "waiting";
     this.timer = 0; 
-
+    // Slight left and right swing while apple are hanging.
     this.swayRate = random(1.0, 3.0);    
     this.swaySpeed = random(0.5, 0.3); 
     this.swayPhase = random(0, TWO_PI); 
   }     
   reset(){
-    //back to the tree.
+    // Reset apple to initial position.
     this.x = this.stratX;
     this.y = this.stratY;
     this.dropSpeed = 0;
@@ -77,20 +79,23 @@ class Apple {
   }
   update(){
     if (this.state ==="waiting"){
+      // Waiting for 2 seconds before falling.
       this.timer++;
       if(this.timer > 120){
         this.state = "falling";
         this.timer = 0;
       }
     } else if (this.state ==="falling"){
+      // Apply gravity.
       this.dropSpeed += gravity * gravityDirection;
       this.y += this.dropSpeed;
-    
+    // Hit ground.
     if(gravityDirection === 1 && this.y >= ground){
       this.y = ground;
       this.state = "landed";
       this.dropSpeed = 0;
       this.timer = 0;
+    // Hit top(when reversed gravity)
     } else if (gravityDirection === -1 && this.y <=topY){
       this.y = topY;
       this.state = "landed";
@@ -99,6 +104,7 @@ class Apple {
     }
   }
       else if (this.state === "landed"){
+        // Rest for 2 seconds then return to tree.
         this.timer++;
         if(this.timer > 120){
           this.reset();
@@ -112,6 +118,7 @@ class Apple {
     
     let drawX = this.x;
     let drawY = this.y;
+  // Slight sway when hanging.
     if (this.state === "waiting") {
       const t = frameCount * this.swaySpeed + this.swayPhase;
       drawX += sin(t) * this.swayRate;}
@@ -120,7 +127,7 @@ class Apple {
       ellipse(drawX, drawY, 40, 40);
   }
 }
-
+//Recursive Tree Generator
 function generateTree(x, y, length, angle, level){
   if (length < 40) return;
   
@@ -131,7 +138,8 @@ function generateTree(x, y, length, angle, level){
 
   let endX = branch.x2;
   let endY = branch.y2;
-  
+
+  //Random chance to grow an apple on highter level branch.
   if (level >= 3 && random()<0.2){
     let t = random(0.3,0.9);
     let appleX = lerp(branch.x, branch.x2, t);
@@ -156,9 +164,10 @@ function generateTree(x, y, length, angle, level){
 
 function setup() {
   createCanvas(windowWidth, windowHeight); 
-  frameRate(60);  
+  frameRate(60);
+  // Keep 600* 800 proportion across screens.  
   scaleFactor = min(windowWidth/ DESIGN_W, windowHeight/ DESIGN_H);
-
+  // Generate background noise lines.
   for (let i = 0; i < 1000; i++){
     noisePoints.push({
       x: random(-width- 1000, width+ 1000),
@@ -166,24 +175,25 @@ function setup() {
       c:[random(100,180), random(150,200), random(200,255), random(80,150)]
     });
   }
+  // Generate a full recursive tree.
   generateTree(300, 650, 200, PI / 2, 1);
 }
 
-
+// Main draw loop
 function draw(){
-  //base background
+  //base blue background.
   background(60,80,120);
   
   push();
   scale(scaleFactor);
   translate((width / scaleFactor - DESIGN_W)/ 2, (height/ scaleFactor - DESIGN_H)/2);
-
+  // Draw background noise.
   noStroke();
   for (let p of noisePoints){
     fill(p.c[0],p.c[1],p.c[2],p.c[3]);
     rect(p.x, p.y, 100, 2);
   }
-
+  // Ground.
   fill(40,140,90);
   rect(0,650,600,100);
   stroke(0);
@@ -192,14 +202,14 @@ function draw(){
   rect(0,650,600,100);
   noStroke();
 
-  //yellow base
+  // Yellow base
   fill(240,210,60);
   stroke(0);
   strokeWeight(10);
   rect(125,625,350,75);
   noStroke();
 
-  //colorfull rects
+  // Colorfull rects
   const colors = [
     color(240,210,60),
     color(240,70,70),
@@ -212,7 +222,7 @@ function draw(){
   const startY = 625;
   const boxW = 350 / 6;
   const boxH = 75;
-
+ 
   for (let i = 0; i < 6; i++){
     fill(colors[i]);
     rect(startX + i * boxW, startY, boxW, boxH);
@@ -260,16 +270,16 @@ function draw(){
     fill(255,255,255,random(10,40));
     rect(random(125,475),random(625,700), 1, 1);
   }
-
+  // Draw all branches.
   for (let branch of branches ){
     branch.draw(); 
   }
-
+  // Draw and update all apples.
   for (let a of apples){
     a.update();
     a.draw();
   }
-  
+  // Display gravity control text.
   noStroke();
   fill(255);
   textSize(25);
@@ -282,11 +292,12 @@ function draw(){
     pop();
 }
 
+// Make the canvas size match the screen size.
 function fitWidow(){
   resizeCanvas(windowWidth,windowHeight);
   scaleFactor = min(windowWidth / DESIGN_W, windowHeight/ DESIGN_H);
 }
-
+// Add keypress to control the direction of gravity.
 function keyPressed(){
   if (key === ' '){
     gravityDirection *= -1;
